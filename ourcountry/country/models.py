263bd3f0d17e2f9659049cc638b4c6bbb52e2591
BekturKochorbaev <bekturkochorbaev64@gmail.com>
2024-12-15 11:41:06 +0600

    test

--- ourcountry/country/models.py
+++ ourcountry/country/models.py
@@ -7,21 +7,3 @@ class UserProfile(AbstractUser):
     phone_number = PhoneNumberField(region='KG', null=True, blank=True)
 
 
-class Home(models.Model):
-    name = models.CharField(max_length=25)
-    image = models.ImageField(upload_to='home_image/', null=True, blank=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-
-class Regions(models.Model):
-    name = models.CharField(max_length=32)
-    description = models.TextField()
-    regions_image = models.ImageField(upload_to='regions_image/')
-
-xdrcftvgybhunji
-xdfghjk
-fvghbjkhbjnk
-hbjnk
