affe8d2eaf76673ab95a097c31200608b558f716
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2024-12-15 11:28:23 +0600

    hotel

--- ourcountry/country/models.py
+++ ourcountry/country/models.py
@@ -5,3 +5,9 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class UserProfile(AbstractUser):
     phone_number = PhoneNumberField(region='KG', null=True, blank=True)
+
+
+class Regions(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    regions_image = models.ImageField(upload_to='regions_image/')
