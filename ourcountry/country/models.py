46d59383ca0207d56729a398f3934314828d311d
BekturKochorbaev <bekturkochorbaev64@gmail.com>
2024-12-15 14:37:29 +0600

    my commit

--- ourcountry/country/models.py
+++ ourcountry/country/models.py
@@ -6,6 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 class UserProfile(AbstractUser):
     phone_number = PhoneNumberField(region='KG', null=True, blank=True)
 
+
 class Home(models.Model):
     name = models.CharField(max_length=25)
     image = models.ImageField(upload_to='home_image/', null=True, blank=True)
@@ -20,9 +21,3 @@ class Regions(models.Model):
     description = models.TextField()
     regions_image = models.ImageField(upload_to='regions_image/')
 
-xdrcftvgybhunji
-xdfghjk
-fvghbjkhbjnk
-hbjnk
-asdsf
-afdsv
