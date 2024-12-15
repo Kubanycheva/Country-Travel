adc37caa46ef9aa234a057a00e8c400aabb4e9a7
BekturKochorbaev <bekturkochorbaev64@gmail.com>
2024-12-15 11:26:48 +0600

    class Home

--- ourcountry/country/models.py
+++ ourcountry/country/models.py
@@ -5,3 +5,13 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class UserProfile(AbstractUser):
     phone_number = PhoneNumberField(region='KG', null=True, blank=True)
+
+
+class Home(models.Model):
+    name = models.CharField(max_length=25)
+    image = models.ImageField(upload_to='home_image/', null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
