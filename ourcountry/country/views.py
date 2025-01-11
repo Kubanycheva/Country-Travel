7c23cb065d5e50541a61cf48cde7b0ae1949fe9a
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-01-11 11:34:09 +0600

    cool

--- ourcountry/country/views.py
+++ ourcountry/country/views.py
@@ -349,11 +349,11 @@ class FavoriteItemViewSet(viewsets.ModelViewSet):
 class FavoriteItemViewSet(viewsets.ModelViewSet):
     serializer_class = FavoriteItemSerializers
 
+
     def get_queryset(self):
         return FavoriteItem.objects.filter(favorite__user=self.request.user)
 
     def perform_create(self, serializer):
-        cart, created = Favorite.objects.get_or_create(user=self.request.user)
+        cart, created = FavoriteItem.objects.get_or_create(user=self.request.user)
         serializer.save(cart=cart)
-
 
